7090852c83e9712df652dd796b58fa11b31c6e2e
adity <adityanex2003@gmail.com>
2025-12-14 12:18:42 +0530

    initial upload

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pickle
 import pandas as pd
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -60,5 +61,8 @@ def predict():
     except Exception as e:
         return str(e)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
